dd9164a921d3ddc1b7155e816635cae6ada58413
Douglas Vasconcelos <doug.impactone@gmail.com>
2025-11-16 02:46:00 -0300

    Exercício 01 feito. Porém, apresenta erro ao tentar converter argumento

---
+++ logica_programacao/01_numeros_pares_impares.py
@@ -0,0 +1,7 @@
+numero = input("Digite um número: ") #variável que salva o número com a respota do usuário
+if numero % 2 == 0: # Se o número que o usuário digitar for divisível por 2 e restar zero retorna a mensagem abaixo
+    print("O número é par!")
+else: # se não retorna a mensagem abaixo
+    print("O número é ímpar!")    
+
+# Quando tento rodar no terminal retorna o erro: not all arguments converted during string formatting
