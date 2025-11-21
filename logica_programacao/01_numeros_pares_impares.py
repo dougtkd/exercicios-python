b5c692f3553297a99b2d4f7dfa4fbf1287c0f470
Douglas Vasconcelos <doug.impactone@gmail.com>
2025-11-20 22:09:29 -0300

    atualização do código rodando, foi adicionado a conversão da variavel numero de string para inteiro!

--- logica_programacao/01_numeros_pares_impares.py
+++ logica_programacao/01_numeros_pares_impares.py
@@ -1,7 +1,8 @@
-numero = input("Digite um número: ") #variável que salva o número com a respota do usuário
+numero = int(input("Digite um número: ")) #variável que salva o número com a respota do usuário
 if numero % 2 == 0: # Se o número que o usuário digitar for divisível por 2 e restar zero retorna a mensagem abaixo
     print("O número é par!")
 else: # se não retorna a mensagem abaixo
     print("O número é ímpar!")    
 
 # Quando tento rodar no terminal retorna o erro: not all arguments converted during string formatting
+## Descobri que o problema era por conta da variavel numero ser string e é necessário converter ela para inteiro, logo acrescentei o int antes do input e rodou!
